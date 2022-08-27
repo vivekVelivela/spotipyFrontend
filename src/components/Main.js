4f1fb3cd913236c3d59cacc269234bad6d06bce9
VivekVelivela <vivek.velivela@dataarmy.io>
2022-08-27 12:18:07 +1000

    fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,13 @@ import React from 'react';
 import { useState } from 'react';
 import {Navbar} from ".//Navbar/Navbar"
 import BarChart from './/BarChart';
-import {UserData} from './/Data'
+import { userData } from './/Data'
 function Main(){
   const [userData] = useState({
-    labels:UserData.map((data)=> data.name),
+    labels:userData.map((data)=> data.name),
     datasets: [{
       label: "Users Gained",
-      data: UserData.map((data) => data.followers)
+      data: userData.map((data) => data.followers)
     }]
   })
 

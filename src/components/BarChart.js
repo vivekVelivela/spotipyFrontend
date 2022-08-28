40a93b43f5589b64cdc8535605306d67ff728e53
VivekVelivela <vivek.velivela@dataarmy.io>
2022-08-28 12:12:02 +1000

    chart

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
+// BarChart.defaults.scale.gridLines.drawOnChartArea = false;
 
-function BarChart({ chartData }) {
-  return <Bar data={chartData} />;
+function BarChart({ chartData} ) 
+{
+  return <Bar data={chartData} options={{responsive: true,  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+    },
+    y: {
+      grid: {
+        display: false,
+      },
+    },
+  }
+}}/>;
 }
 
 export default BarChart;
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,21 +5,44 @@ import {Navbar} from ".//Navbar/Navbar";
 import BarChart from './/BarChart';
 import { playlist_followers } from './/Data'
 import { track_popularity } from ".//Data";
+import { Container } from '@mui/material'
+import Grid from '@mui/material/Grid'; // Grid version 1
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+
 function Main(){
   const [UserData] = useState({
     labels:playlist_followers.map((data)=> data.name),
     datasets: [{
       label: "Followers",
-      data: playlist_followers.map((data) => data.followers)
-    }]
-  })
+      data: playlist_followers.map((data) => data.followers),
+      backgroundColor: [
+        "rgba(75,192,192,1)",
+        "#ecf0f1",
+        "#50AF95",
+        "#f3ba2f",
+        "#2a71d0",
+      ],
+      borderColor: "white",
+      borderWidth: 1,
+    },
+  ],
+  });
+  
 
   return(
-    <div className='Main'>
+    
+    <div className='background-red'>
       <Navbar></Navbar>
-      <div style={{width: 700}}>
-      <BarChart chartData={UserData}/>
-      </div>
+      <Grid >
+        <Grid className = 'container'>
+        {/* <div className='BarChart'> */}
+          <BarChart chartData={UserData} />
+        {/* </div> */}
+        </Grid>
+        <Grid>
+          <BarChart chartData={UserData}/>
+        </Grid>
+      </Grid>
     </div>
   )
 }

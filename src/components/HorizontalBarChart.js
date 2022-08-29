c5c8c82b3faa4fd44ae8abe1f42ef7017f42e1c6
VivekVelivela <vivek.velivela@dataarmy.io>
2022-08-29 22:30:50 +1000

    new changes

---
+++ src/components/HorizontalBarChart.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+
+
+function HorizontalBarChart({ chartData } ) 
+{
+  return <Bar data={chartData} options={{responsive: true, indexAxis: 'y', scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+    },
+    y: {
+      grid: {
+        display: false,
+      },
+    },
+  },
+  plugins: {title: {
+    display: true,
+    align: 'centre',
+    text: 'Track Followers',
+    font: {
+      size: 25
+    }
+}}
+}}/>;
+}
+
+export default HorizontalBarChart;
